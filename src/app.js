a76c4fcee325c4fb5bbb0144b01b1d4cbadcaa14
dachan <dachankim@parastar.co.kr>
2026-09-11 00:28:54 +0900

    fix: app.js / server.js 분할

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import { errorHandler } from '#src/middlewares/error-handler.middleware.js';
+import { logger } from '#src/middlewares/logger.js';
+import cors from 'cors';
+import express from 'express';
+import { isDevelopment } from './config/config.js';
+import { router } from './routes/index.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+if (isDevelopment) {
+  app.use(logger);
+}
+
+app.use('/api', router);
+app.use(errorHandler);
+
+export { app };
--- src/server.js
+++ src/server.js
@@ -1,22 +1,5 @@
-import { errorHandler } from '#src/middlewares/error-handler.middleware.js';
-import { logger } from '#src/middlewares/logger.js';
-import cors from 'cors';
-import express from 'express';
-import { config, isDevelopment } from './config/config.js';
-import { router } from './routes/index.js';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-if (isDevelopment) {
-  app.use(logger);
-}
-
-app.use('/api', router);
-
-app.use(errorHandler);
+import { app } from './app.js';
+import { config } from './config/config.js';
 
 app.listen(config.PORT, () => {
   console.log(`🚀 Server is listening on port ${config.PORT}`);
