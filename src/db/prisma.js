cb84c79881cb2c035af79476a6ac0fecad848c85
dachan <dachankim@parastar.co.kr>
2026-09-10 16:20:45 +0900

    feat: db

---
+++ src/db/prisma.js
@@ -0,0 +1,23 @@
+import { config } from '#config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/index.js';
+
+const adapter = new PrismaPg({
+  connectionString: config.DATABASE_URL,
+});
+
+const globalForPrisma = globalThis;
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
