c93bab15c4d2bc53ee058f4591ae8e722bb839d2
dachan <dachankim@parastar.co.kr>
2026-09-11 17:45:45 +0900

    chore:test

--- src/app.js
+++ src/app.js
@@ -17,4 +17,7 @@ if (isDevelopment) {
 app.use('/api', router);
 app.use(errorHandler);
 
-export default app;
+// export default app;
+export default function handler(req, res) {
+  return app(req, res);
+}
