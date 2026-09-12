a9f5b7ca1afa76e4ba0963c409a467317b0c16ef
dachan <dachankim@parastar.co.kr>
2026-09-12 14:22:59 +0900

    chore: conflict 복구

--- src/controllers/study.controller.js
+++ src/controllers/study.controller.js
@@ -78,6 +78,21 @@ export const deleteStudy = async (req, res, next) => {
   }
 };
 
-export const createReaction = async (req, res, next) => {};
-
+export const createReaction = async (req, res, next) => {
+  try {
+    const studyId = req.validated.params.study_id;
+    const body = req.validated.body;
+    const reaction = await studyService.handleReactionToggleService(
+      studyId,
+      body,
+    );
+    res.status(HTTP_STATUS.CREATE).json({
+      success: true,
+      data: reaction,
+      message: '리액션이 생성되었습니다.',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 export const updatePoints = (req, res, next) => {};
