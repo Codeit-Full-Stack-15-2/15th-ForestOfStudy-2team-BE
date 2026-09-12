16ccf33b51534e5ec4f5cc2acb3f94598eb03ac5
dachan <dachankim@parastar.co.kr>
2026-09-12 09:36:59 +0900

    chore: server running message fix

--- src/app.js
+++ src/app.js
@@ -15,7 +15,55 @@ if (isDevelopment) {
 }
 app.use('/api', router);
 // vercel-heath-check
-app.get('/', (req, res) => res.send('API Server is successfully running!'));
+app.get('/', (req, res) => {
+  const pikachu = `
+quu..__
+ $$$b   \`---.__
+  "$$b         \`--.                           ___.---uuudP
+   \`$$b            \`.__.------.__     __.---'      $$$$"              .
+     "$b          -'            \`-.-'            $$$"              .'|
+       ".                                       d$"              _.'  |
+         \`.   /                               ..."              .'     |
+           \`./                           ..::-'             _.'       |
+            /                         .:::-'            .-'         .'
+           :                          ::''\\          _.'            |
+          .' .-.             .-.            \`.      .'                |
+          : /'$$|           .@"$\\            \`.   .'              _.-'
+         .'|$u$$|          |$$,$$|            |  <             _.-'
+         | \`:$$:'          :$$$$$:            \`.  \`.        .-'
+         :                  \`"--'              |    \`-.     \\
+        :##.       ==             .###.        \`.      \`.    \`\\
+        |##:                      :###:        |        >     >
+        |#'     \`..'\`..'          \`###'        x:      /     /
+         \\                                    xXX|     /    ./
+          \\                                 xXXX'|    /   ./
+          /\`-.                                   \`.  /   /
+         :    \`-  ...........,                    | /  .'
+         |         \`\`:::::::'        .            |<    \`.
+         |             \`\`\`          |            x| \\ \`.:\`\`.
+         |                          .'    /'   xXX|  \`:\`M\`M':.
+         |    |                    ;    /:' xXXX'|  -'MMMMM:'
+         \`.  .'                    :    /:'       |-'MMMM.-'
+          |  |                    .'   /'        .'MMM.-'
+          \`'\`'                    :  ,'          |MMM<
+            |                      \`'            |tbap\\
+             \\                                   :MM.-'
+              \\                               |              .''
+               \\.               \`.            /
+                /     .:::::::.. :           /
+               |     .:::::::::::\`.         /
+               |   .:::------------\\       /
+              /   .''                >::'  /
+              \`',:                  :    .'
+                                    \`:.:'
+`;
+  res.send(`
+    <pre style="font-family: monospace; font-size: 13px; line-height: 1.1; color: #f59e0b; background-color: #111; padding: 24px; border-radius: 8px; display: inline-block;">
+${pikachu}
+⚡ Pika-Pika! API Server & Supabase DB are successfully running! ⚡
+    </pre>
+  `);
+});
 app.use(errorHandler);
 
 export default app;
