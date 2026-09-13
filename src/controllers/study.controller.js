0d5a4c33070bc9f97047f6a3550eeee5e0858338
thcjfd <thcjfd@naver.com>
2026-09-13 23:43:22 +0900

    feat: point API 추가

--- src/controllers/study.controller.js
+++ src/controllers/study.controller.js
@@ -1,4 +1,5 @@
 import * as studyService from '#src/services/study.service.js';
+import { success } from 'zod';
 import { HTTP_STATUS } from '../constants/index.js';
 
 export const getStudies = (req, res, next) => {};
@@ -61,9 +62,18 @@ export const getStudy = async (req, res, next) => {
   }
 };
 
-export const updatePoints = async(req, res, next)=>{
+export const updatePoints = async(req, res)=>{
   const study_id = req.validated.params.study_id;
+  const minutes = req.validated.body.minutes;
+  const result = await studyService.addPointService(study_id, minutes);
+
+  res.status(HTTP_STATUS.OK).json({
+    success: true,
+    data: result,
+    message: '포인트 수정 성공',
+  })
 }
+
 export const updateStudy = (req, res, next) => {};
 
 export const deleteStudy = async (req, res, next) => {
@@ -83,7 +93,7 @@ export const deleteStudy = async (req, res, next) => {
 
 export const createReaction = async (req, res, next) => {
   try {
-    const studyId = req.validated.params.study_id;
+    
     const body = req.validated.body;
     const reaction = await studyService.handleReactionToggleService(
       studyId,
@@ -97,4 +107,5 @@ export const createReaction = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+
 };
