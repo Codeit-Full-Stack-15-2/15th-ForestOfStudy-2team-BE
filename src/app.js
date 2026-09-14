9f159e3e1c41c61b52a06cd9bf10bd8cbcdcfd78
YANGPA <wodud7273@gmail.com>
2026-09-15 01:04:53 +0900

    fix 프론트-버셀 연결전 테스트 커밋

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import { swaggerUI,swaggerSpec } from '#src/swagger/swagger.js';
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://forest-of-study-chi.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
