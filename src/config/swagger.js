d3ec0bf617481d48b03925bd2f51251f62a38320
dachan <dachankim@parastar.co.kr>
2026-09-17 01:00:53 +0900

    refactor: swagger 세팅

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,9 +1,5 @@
-import path from 'path';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUI from 'swagger-ui-express';
-
-// process.cwd() 기반의 절대 경로 생성
-const docsDir = path.join(process.cwd(), 'src/swagger/docs');
 
 const options = {
   definition: {
@@ -22,11 +18,6 @@ const options = {
       },
     ],
   },
-  // 하위 폴더(studies 등)의 YAML 파일까지 절대 경로 와일드카드로 매핑
-  // apis: [
-  //   path.join(docsDir, '**/*.yaml'),
-  //   path.join(docsDir, '**/*.yml'),
-  // ],
   apis: ['src/swagger/docs/**/*.yaml', 'src/swagger/docs/**/*.yml'],
 };
 
